56c4f9e84657883b6fdf0147c4b2d1adafbdc1ee
Jethro Landa <jemaq_lan@hotmail.com>
2026-02-23 08:47:35 +0800

    initial commit

---
+++ sp-ai-reporting.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Plugin Name: Sportspress AI Reporting
+ * Description: Sportspress AI Reporting is a WordPress plugin that provides AI-powered reporting and analytics for sports teams and organizations. It offers insights into player performance, team dynamics, and game strategies, helping coaches and analysts make informed decisions.
+ * Version: 1.0
+ * Author: Jethrolanda
+ * Author URI: https://jethrolanda.com/
+ * Text Domain: wp-plugin-boilerplate
+ * Domain Path: /languages/
+ * Requires at least: 5.7
+ * Requires PHP: 7.2
+ */
+
+defined('ABSPATH') || exit;
+
+// Path Constants ======================================================================================================
+
+define('SPAR_PLUGIN_URL',             plugins_url() . '/sp-ai-reporting/');
+define('SPAR_PLUGIN_DIR',             plugin_dir_path(__FILE__));
+define('SPAR_CSS_ROOT_URL',           SPAR_PLUGIN_URL . 'css/');
+define('SPAR_JS_ROOT_URL',            SPAR_PLUGIN_URL . 'js/');
+define('SPAR_TEMPLATES_ROOT_URL',     SPAR_PLUGIN_URL . 'templates/');
+define('SPAR_TEMPLATES_ROOT_DIR',     SPAR_PLUGIN_DIR . 'templates/');
+define('SPAR_BLOCKS_ROOT_URL',        SPAR_PLUGIN_URL . 'blocks/');
+define('SPAR_BLOCKS_ROOT_DIR',        SPAR_PLUGIN_DIR . 'blocks/');
+
+// Require autoloader
+require_once 'inc/autoloader.php';
+
+// Run
+require_once 'sp-ai-reporting.plugin.php';
+$GLOBALS['spar'] = new SP_AI_Reporting();
---
+++ sp-ai-reporting.plugin.php
@@ -0,0 +1,80 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+// Exit if accessed directly
+
+
+class SP_AI_Reporting
+{
+
+	/*
+    |------------------------------------------------------------------------------------------------------------------
+    | Class Members
+    |------------------------------------------------------------------------------------------------------------------
+     */
+	private static $_instance;
+
+	public $scripts;
+	public $ajax;
+	public $shortcode;
+	public $rest;
+
+	const VERSION = '1.0';
+
+	/*
+  |------------------------------------------------------------------------------------------------------------------
+  | Mesc Functions
+  |------------------------------------------------------------------------------------------------------------------
+  */
+
+	/**
+	 * Class constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct()
+	{
+
+		$this->scripts = SPAR\Plugin\Scripts::instance();
+		$this->ajax = SPAR\Plugin\Ajax::instance();
+		$this->shortcode = SPAR\Plugin\Shortcode::instance();
+		$this->rest = SPAR\Plugin\Rest::instance();
+
+		// Register Activation Hook
+		register_activation_hook(SPAR_PLUGIN_DIR . 'sp-ai-reporting.php', array($this, 'activate'));
+
+		// Register Deactivation Hook
+		register_deactivation_hook(SPAR_PLUGIN_DIR . 'sp-ai-reporting.php', array($this, 'deactivate'));
+	}
+
+	/**
+	 * Singleton Pattern.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function instance()
+	{
+
+		if (!self::$_instance instanceof self) {
+			self::$_instance = new self;
+		}
+
+		return self::$_instance;
+	}
+
+
+	/**
+	 * Trigger on activation
+	 *
+	 * @since 1.0.0
+	 */
+	public function activate() {}
+
+	/**
+	 * Trigger on deactivation
+	 *
+	 * @since 1.0.0
+	 */
+	public function deactivate() {}
+}
